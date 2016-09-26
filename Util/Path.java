e22b21a3e44da80e3a2bd9406c2d773392107447
Olverine <oliver.david.norin@gmail.com>
2016-09-26 13:51:09 +0200

    PATH grejer

--- Util/Path.java
+++ Util/Path.java
@@ -1,5 +1,37 @@
 package game.AntiTdGame.Util;
 
+import java.util.ArrayList;
+
+import engine.Common.Vector;
+
 public class Path {
+	ArrayList<Vector> nodes = new ArrayList<Vector>();
 	
+	public Path (ArrayList<Vector> nodes) {
+		this.nodes = nodes;
+	}
+	
+	public Vector getPositionAtLength(double length) {
+		
+		int i = 0;
+		while(true){
+			if(length < distance(nodes.get(i), nodes.get(i + 1))){
+				break;
+			}
+			i++;
+		}
+		
+		
+		
+		return new Vector(0, 0);
+	}
+	
+	double distance(Vector v1, Vector v2){
+		double a = (v1.getX() - v2.getY());
+		double b = (v1.getY() - v2.getY());
+		
+		double c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+		
+		return c;
+	}
 }
