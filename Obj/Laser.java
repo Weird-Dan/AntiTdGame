d03f6f40c0d7aac7aebe8a86b0d40c3505b42c53
Weird-Dan <t4.daniel.grenehed@umea.nti.se>
2016-09-29 09:44:39 +0200

    projectiles
    burk

---
+++ Obj/Laser.java
@@ -0,0 +1,36 @@
+package game.AntiTdGame.Obj;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import engine.Common.Vector;
+import game.AntiTdGame.Level;
+
+public class Laser extends Projectile {
+
+	final long ct;
+
+	public Laser(Actor a, Vector pos, int damage, Level level) {
+		this.target = a;
+		this.pos = pos;
+		this.dmg = damage;
+		this.level = level;
+		this.ct = System.currentTimeMillis();
+	}
+
+	public void paint(Graphics g) {
+		g.setColor(Color.red);
+		g.drawLine((int) target.pos.getX(), (int) target.pos.getY(), (int) pos.getX(), (int) pos.getY());
+	}
+
+	public void update() {
+		if (getLifetime() > 900) {
+			// die, damage unit
+		}
+	}
+
+	public long getLifetime() {
+		return System.currentTimeMillis() - this.ct;
+	}
+
+}
---
+++ Obj/Projectile.java
@@ -0,0 +1,30 @@
+package game.AntiTdGame.Obj;
+
+import java.awt.Graphics;
+
+import engine.Common.Vector;
+import engine.Render.Renderable;
+import game.AntiTdGame.Level;
+
+public abstract class Projectile implements Renderable {
+
+	Actor target;
+
+	Vector pos;
+	
+	Level level;
+	
+	int dmg;
+
+	@Override
+	public void paint(Graphics g) {
+		// paint projectile
+	}
+
+	@Override
+	public void update() {
+		// move
+		// if on target, damage, and remove self
+	}
+
+}
